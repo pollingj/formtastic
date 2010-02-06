6fde5b4b135175e5c7fde1bc0e299a1057873c6c
John Polling <johnpolling@Macintosh.local>
2010-02-06 08:27:15 +0000

    Tidied up the item_separator spec

--- spec/item_separator_spec.rb
+++ spec/item_separator_spec.rb
@@ -12,13 +12,8 @@ describe 'SemanticFormBuilder#item_separator' do
   
   describe 'when @@item_separator is default (:li)' do 
 
-    it 'should render inputs wrapper in li tags and wrapperd in ol tag' do
-      @bob.stub!(:column_for_attribute).and_return(mock('column', :type => :string, :limit => 255))
-      semantic_form_for(@new_post) do |builder|
-        builder.inputs :for => [:author, @bob] do |bob_builder|
-          concat(bob_builder.input(:login))
-        end
-      end
+    it 'should render inputs and buttons wrapped in li tags and wrapping in ol tag' do
+      render_form
       
       output_buffer.should have_tag("form > fieldset > ol > li")
     end
@@ -26,19 +21,26 @@ describe 'SemanticFormBuilder#item_separator' do
   
   describe 'when @@item_separator is set to :div' do
     
-    it 'should render inputs wrapper in div tags and have not wrapping ol tag' do
+    it 'should render inputs and buttons wrapped in div tags and have not wrapping ol tag' do
       ::Formtastic::SemanticFormBuilder.item_separator = :div
       
-      @bob.stub!(:column_for_attribute).and_return(mock('column', :type => :string, :limit => 255))
-      semantic_form_for(@new_post) do |builder|
-        builder.inputs :for => [:author, @bob] do |bob_builder|
-          concat(bob_builder.input(:login))
-        end
-      end
+      render_form
       
       output_buffer.should have_tag("form > fieldset > div")
       output_buffer.should_not have_tag("form > fieldset > ol")
     end
   end
+  
+  private 
+    def render_form
+      @bob.stub!(:column_for_attribute).and_return(mock('column', :type => :string, :limit => 255))
+      semantic_form_for(@new_post) do |builder|
+        builder.inputs :for => [:author, @bob] do |bob_builder|
+          concat(bob_builder.input(:login))
+          concat(builder.buttons(:commit, :name => "Now click a button", :id => "my-id"))
+          
+        end
+      end
+    end
   
 end
